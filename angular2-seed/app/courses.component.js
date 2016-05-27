07556559292d9e1dd1ab78fb83c9dc445097d9bc
Ryan Engle <rdengle@gmail.com>
2016-05-26 21:00:38 -0400

    Added Courses to template per lesson 12.
    Added 02-a2j to .gitignore

--- angular2-seed/app/courses.component.js
+++ angular2-seed/app/courses.component.js
@@ -21,19 +21,24 @@ System.register(['angular2/core'], function(exports_1, context_1) {
             // decorator function call
             CoursesComponent = (function () {
                 function CoursesComponent() {
+                    this.title = 'The title of courses page';
+                    this.courses = ['Course 1', 'Course 2', 'Course 3'];
                 }
                 CoursesComponent = __decorate([
                     core_1.Component({
                         // selector: specifies a CSS selector for a host HTML element
                         // when angular sees an element that matches the CSS selector,
                         // it will create an instance of our component in the host element
-                        // here we are assuming the host element is an element with the tag
-                        // 'courses'
+                        // here we are assuming the host element is an element with the 
+                        // tag 'courses'
                         selector: 'courses',
-                        // template: specifies the HTML that will be inserted into the DOM when the
-                        // component's view is rendered.
-                        // we can write the template inline or put it in a separate file    
-                        template: '<h2>Courses</h2>',
+                        // template: specifies the HTML that will be inserted into the DOM
+                        // when the component's view is rendered.
+                        // we can write the template inline or put it in a separate file
+                        // interpolation: {{ title }} is an example of one-way binding. 
+                        // Updating the title property will cause the component template 
+                        // to be updated automatically    
+                        template: "        \n        <h2>Courses</h2>\n        {{ title }}\n        <ul>\n            <li *ngFor='#course of courses'>\n            {{ course }}\n            </li>\n        </ul>\n        "
                     }), 
                     __metadata('design:paramtypes', [])
                 ], CoursesComponent);
--- angular2-seed/app/courses.component.ts
+++ angular2-seed/app/courses.component.ts
@@ -6,14 +6,26 @@ import {Component} from 'angular2/core'
     // selector: specifies a CSS selector for a host HTML element
     // when angular sees an element that matches the CSS selector,
     // it will create an instance of our component in the host element
-    // here we are assuming the host element is an element with the tag
-    // 'courses'
+    // here we are assuming the host element is an element with the 
+    // tag 'courses'
     selector: 'courses',
-    // template: specifies the HTML that will be inserted into the DOM when the
-    // component's view is rendered.
-    // we can write the template inline or put it in a separate file    
-    template: '<h2>Courses</h2>',
+    // template: specifies the HTML that will be inserted into the DOM
+    // when the component's view is rendered.
+    // we can write the template inline or put it in a separate file
+    // interpolation: {{ title }} is an example of one-way binding. 
+    // Updating the title property will cause the component template 
+    // to be updated automatically    
+    template: `        
+        <h2>Courses</h2>
+        {{ title }}
+        <ul>
+            <li *ngFor='#course of courses'>
+            {{ course }}
+            </li>
+        </ul>
+        `
 })
 export class CoursesComponent {
-    
+    title: string = 'The title of courses page';
+    courses: string[] = ['Course 1', 'Course 2', 'Course 3'];
 }
